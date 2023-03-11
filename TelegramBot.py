85e2cea854d332d13c9720eb815d300a500cefd4
kozlofAlex <kozlaleksan2013@gmail.com>
2023-03-11 08:45:05 +0300

    Final

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,20 +1,45 @@
 import telebot
-TOKEN = "5877938939:AAHOOr7D_HpnMT5eKsVS5Y0qenqYw6vRcM8"
+from Config import TOKEN, values, main_menu, help
+from Extensions import APIException, CryptoConverter
+
 bot = telebot.TeleBot(TOKEN)
 
-# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, f"Welcome, {message.chat.username}")
+@bot.message_handler(commands=['menu'])
+def main_menuu(message):
+    bot.send_message(message.chat.id, main_menu)
 
-# Обрабатываются все сообщения, содержащие фото'.
-@bot.message_handler(content_types=['photo', ])
-def say_lmao(message: telebot.types.Message):
-    bot.reply_to(message, 'Nice meme XDD')
+@bot.message_handler(commands=['start'])
+def startt(message):
+    bot.send_message(message.chat.id, help + '\n /menu')
 
-# Обрабатываются все документы и аудиозаписи
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-    pass
+@bot.message_handler(commands=['help'])
+def helpp(message):
+    bot.send_message(message.chat.id, help + '\n /menu')
+
+@bot.message_handler(commands=['values'])
+def valuess(message):
+    bot.send_message(message.chat.id, 'ДОСТУПНЫ СЛЕДУЮЩИЕ ВАЛЮТЫ:')
+    for i in values:
+        bot.send_message(message.chat.id, i + ' ' + values[i] )
+    bot.send_message(message.chat.id, '/menu')
+
+@bot.message_handler(content_types=['text'])
+def convert_result(message: telebot.types.Message):
+    try:
+        val = message.text.split(' ')
+
+        if len(val) != 3:
+            raise APIException('Слишком много или слишком мало параметров')
+
+        base, quote, amount = val
+        result = CryptoConverter.convert(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n {e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n {e}')
+    else:
+        text = f'{amount} {values[base]}({base}) в {values[quote]}({quote}) равно: {result}'
+        bot.send_message(message.chat.id, text)
+        bot.send_message(message.chat.id, "/menu")
 
 bot.polling(none_stop=True)
